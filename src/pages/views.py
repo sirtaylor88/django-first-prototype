2266cf1fbe5cf2d97d85953a5594ad61253ebcdd
Nhat Tai NGUYEN <nhattai.nguyen88@gmail.com>
2020-07-13 14:36:33 +0200

    Use template tags and filters

--- src/pages/views.py
+++ src/pages/views.py
@@ -13,9 +13,10 @@ def contact_view(request, *args, **kwargs):
 
 def about_view(request, *args, **kwargs):
     my_context = {
-      "my_text": "Nathan NGUYEN",
+      "title": "Nathan NGUYEN",
       "my_number": 123456789,
-      "my_list": [123, 234, 345, "Other"]
+      "my_list": [123, 234, 345, "Other"],
+      "my_html": "<h1>Hello World</h1>"
     }
     return render(request, 'about.html', my_context)
 
