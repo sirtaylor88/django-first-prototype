2d01ae42461236e4975eb7e47a4cba22087a35a5
Nhat Tai NGUYEN <nhattai.nguyen88@gmail.com>
2020-07-13 11:22:43 +0200

    Add page views

---
+++ src/pages/views.py
@@ -0,0 +1,18 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+def home_view(request, *args, **kwargs):
+    print(args, kwargs)
+    print(request.user)
+    # return HttpResponse("<h1>Hello World</h1>")
+    return render(request, 'home.html', {})
+
+def contact_view(request, *args, **kwargs):
+    return render(request, 'contact.html', {})
+
+def about_view(request, *args, **kwargs):
+    return render(request, 'about.html', {})
+
+def social_view(request, *args, **kwargs):
+    return HttpResponse("<h1>Social Page</h1>")
--- src/trydjango/settings.py
+++ src/trydjango/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     # own
+    'pages',
     'products',
 ]
 
@@ -57,7 +58,7 @@ ROOT_URLCONF = 'trydjango.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, "templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
