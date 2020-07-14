cd33f5b60d123b7a459c7ec3edc99adeae01865f
Nhat Tai NGUYEN <nhattai.nguyen88@gmail.com>
2020-07-14 11:20:58 +0200

    Add validations for forms

--- src/products/views.py
+++ src/products/views.py
@@ -11,19 +11,19 @@ def product_detail_view(request):
     return render(request, "products/product_detail.html", context)
 
 def product_create_view(request):
-    form = RawProductForm()
-    if request.method == "POST":
-        form = RawProductForm(request.POST)
-        if form.is_valid():
-            print(form.cleaned_data)
-            Product.objects.create(**form.cleaned_data)
-        else:
-            print(form.errors)
+    # form = RawProductForm()
+    # if request.method == "POST":
+    #     form = RawProductForm(request.POST)
+    #     if form.is_valid():
+    #         print(form.cleaned_data)
+    #         Product.objects.create(**form.cleaned_data)
+    #     else:
+    #         print(form.errors)
 
-    # form = ProductForm(request.POST or None)
-    # if form.is_valid():
-    #     form.save()
-    #     form = ProductForm
+    form = ProductForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        form = ProductForm
     context = {
       'form': form
     }
